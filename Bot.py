ebea5f07cf6896805d30455ba74ea87fd5a266b8
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2025-06-15 21:02:41 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -6,7 +6,7 @@ from telegram.ext import (
     ContextTypes,
 )
 import os
-BOT_TOKEN = os.environ["7628097563:AAFvePAB1EkfCMDWywZ6NM-il7oFlIiJpZc"]
+BOT_TOKEN = "7628097563:AAFvePAB1EkfCMDWywZ6NM-il7oFlIiJpZc"
 
 
 # Main keyboard
